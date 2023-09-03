5f9d357c36841d1c671521e26e93d35e9ec84e75
Md Farid Uddin Kiron <kironiitdu@outlook.com>
2023-09-03 20:04:33 +0800

    Basic project architecture

---
+++ HireExpat/Data/ApplicationDbContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Net;
+using System.Reflection.Emit;
+using System.Xml.Linq;
+
+namespace HireExpat.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+       
+
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+           
+
+        }
+    }
+}
