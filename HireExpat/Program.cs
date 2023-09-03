044f2879cf0eb76c009d9b172f1f259ae340b1f5
Md Farid Uddin Kiron <kironiitdu@outlook.com>
2023-09-03 20:28:29 +0800

    Login page direction

--- HireExpat/Program.cs
+++ HireExpat/Program.cs
@@ -22,6 +22,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Home}/{action=Index}/{id?}");//Change here and introduce your login page
 
 app.Run();
