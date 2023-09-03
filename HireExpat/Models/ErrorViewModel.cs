45e692164052dfa3caafdebbeb3be13a1f3d02db
Md Farid Uddin Kiron <kironiitdu@outlook.com>
2023-09-03 19:28:05 +0800

    Add project files.

---
+++ HireExpat/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HireExpat.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
